9c69910fc96ccd01ae72d546c15104d8ac377339
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2023-03-23 16:39:11 +0100

    additional content variant 1

--- page/conf.py
+++ page/conf.py
@@ -68,6 +68,115 @@ html_context = {
         },
         "Help": "https://github.com/orgs/starlite-api/discussions",
     },
+    "info_cards": [
+        {
+            "title": "Fully featured",
+            "content": (
+                "Everything that's needed to build modern APIs, from data serialization and validation to websockets, "
+                "ORM integration, session management, authentication and more"
+            ),
+            "icon": None,
+            "link": None,
+        },
+        {
+            "title": "Fast",
+            "content": (
+                "Starlite puts great emphasis on developer experience and performance: It's one of the fastest "
+                "ASGI frameworks and developing with it is just as fast"
+            ),
+            "icon": None,
+            "link": "https://docs.starliteproject.dev/latest/benchmarks",
+        },
+        {
+            "title": "(a)synchronous",
+            "content": (
+                "Asynchronous at heart, but with synchronous execution not as a second class citizen: "
+                "Synchronous applications run without performance penalties"
+            ),
+            "icon": None,
+            "link": None,
+        },
+        {
+            "title": "Community driven",
+            "content": (
+                "Developed by a community of volunteers, and led by a team of maintainers, Starlite is a "
+                "community project at heart"
+            ),
+            "icon": None,
+            "link": "https://starliteproject.dev/about/organization.html",
+        },
+        {
+            "title": "Opinionated yet flexible",
+            "content": "Developed to encourage best practices, but flexible enough to stay out of your way",
+            "icon": None,
+            "link": None,
+        },
+        {
+            "title": "Scalable",
+            "content": (
+                "Bottom-up architecture makes it easy to develop modular and decoupled code, allowing "
+                "applications to scale easily, both in development and deployment"
+            ),
+            "icon": None,
+            "link": None,
+        },
+    ],
+    "feature_cards": [
+        {
+            "title": "Data validation and serialization",
+            "content": (
+                "Validation of incoming and serialization of outgoing data, configured with"
+                "type hints with support for libraries such as Pydantic or attrs"
+            ),
+            "link": "https://docs.starliteproject.dev/2/usage/dto.html",
+        },
+        {
+            "title": "OpenAPI",
+            "content": (
+                "Automatic OpenAPI schemas created based on from route handlers and type annotations, "
+                "and options to export them as TypeScript "
+            ),
+            "link": "https://docs.starliteproject.dev/2/usage/openapi.html",
+        },
+        {
+            "title": "Caching",
+            "content": "Response caching with minimal configuration and overhead to speed up applications",
+            "link": "https://docs.starliteproject.dev/2/usage/caching.html",
+        },
+        {
+            "title": "Data stores",
+            "content": (
+                "Interfaces for various key/value stores that seamlessly integrate with "
+                "features such as server-side sessions or response caching, easily managed and organized into "
+                "hierarchical structures"
+            ),
+            "link": "https://docs.starliteproject.dev/2/usage/stores.html",
+        },
+        {
+            "title": "Middlewares",
+            "content": (
+                "Rate-limiting, CORS, CSRF, client- and server-side sessions, compression, logging and many more "
+                "features are all readily available via built in middlewares"
+            ),
+            "link": "https://docs.starliteproject.dev/2/usage/middleware/builtin-middleware.html",
+        },
+        {
+            "title": "ORM integration",
+            "content": (
+                "First-class SQLAlchemy support via DTO backends and plugins that seamlessly integrates with data"
+                "validation and parsing: Models can be returned straight away or used to validate incoming data"
+            ),
+            "link": "https://docs.starliteproject.dev/2/usage/plugins/sqlalchemy.html",
+        },
+        {
+            "title": "Dependency injection",
+            "content": (
+                "Powerful dependency injection on all application layers, aides in code decoupling and reduces "
+                "repetition"
+            ),
+            "link": "https://docs.starliteproject.dev/2/usage/dependency-injection.html",
+        },
+    ],
     "announcement": {
         "title": "Starlite 2.0.0alpha1 is available",
         "description": "Check out the latest release here",
