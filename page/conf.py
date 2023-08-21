6cb72bfe06db1176cb20a05cb6e9d66fe8068b93
Janek Nouvertné <25355197+provinzkraut@users.noreply.github.com>
2023-08-21 16:47:26 +0200

    update announcement

--- page/conf.py
+++ page/conf.py
@@ -169,9 +169,9 @@ html_context = {
         },
     ],
     "announcement": {
-        "title": "Litestar 2.0.0rc1 is available",
-        "description": "Check out the latest release here",
-        "link": "https://github.com/litestar-org/litestar/releases/tag/v2.0.0rc1",
+        "title": "Litestar 2.0 has just been released!",
+        "description": "Check it out here",
+        "link": "https://github.com/litestar-org/litestar/releases/tag/v2.0.0",
     },
     "project_name": "Litestar",
     "project_url": "https://litestar.dev",
