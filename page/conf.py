b337caf09308282a4f0acc958d30d1eb1ee465f5
Jacob Coffee <jacob@z7x.org>
2024-08-27 21:04:34 -0500

    Update Litestar version to 2.11.0 (#44)

--- page/conf.py
+++ page/conf.py
@@ -190,9 +190,9 @@ html_context = {
         },
     ],
     "announcement": {
-        "title": "Litestar 2.10.0 has just been released!",
+        "title": "Litestar 2.11.0 has just been released!",
         "description": "Check it out here",
-        "link": "https://docs.litestar.dev/latest/release-notes/changelog.html#2.10.0",
+        "link": "https://docs.litestar.dev/latest/release-notes/changelog.html#2.11.0",
     },
     "project_name": "Litestar",
     "project_url": "https://litestar.dev",
