755fa5dd0b5550aee6f26288c7085d6547850f10
Harshal Laheri <73422191+Harshal6927@users.noreply.github.com>
2025-05-07 00:19:36 -0400

    update litestar version to 2.16.0

--- page/conf.py
+++ page/conf.py
@@ -190,9 +190,9 @@ html_context = {
         },
     ],
     "announcement": {
-        "title": "Litestar 2.15.2 has been released!",
+        "title": "Litestar 2.16.0 has been released!",
         "description": "Check it out here",
-        "link": "https://docs.litestar.dev/latest/release-notes/changelog.html#2.15.2",
+        "link": "https://docs.litestar.dev/latest/release-notes/changelog.html#2.16.0",
     },
     "project_name": "Litestar",
     "project_url": "https://litestar.dev",
